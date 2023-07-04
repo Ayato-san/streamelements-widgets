d34828fb31f8d2bda632eb80dc2662a8aa10c052
Ayato-san <ayatosan.pro@gmail.com>
2023-07-04 14:28:38 +0200

    feat (demo) : final build
    final build for the demo widget

--- demo/widget.js
+++ demo/widget.js
@@ -1,15 +1,1 @@
-const tchat = document.querySelector('#tchat')
-
-let fieldData = {}
-window.addEventListener('onWidgetLoad', function (obj) {
-    fieldData = obj.detail.fieldData
-    console.log(fieldData)
-})
-window.addEventListener('onEventReceived', function (obj) {
-    const data = obj.detail.event
-    if (data.listener === 'widget-button') {
-        if (data.field === 'someButton' && data.value === 'Thanks') {
-            console.log('button click')
-        }
-    }
-})
+const tchat=document.querySelector("#tchat");let fieldData={};window.addEventListener("onWidgetLoad",(function(e){fieldData=e.detail.fieldData,console.log(fieldData)})),window.addEventListener("onEventReceived",(function(e){const t=e.detail.event;"widget-button"===t.listener&&"someButton"===t.field&&"Thanks"===t.value&&console.log("button click")}));
